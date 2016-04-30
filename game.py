500279216e8a9237aa654f0c3b0219a6739cc8b2
Enrique Bonilla <Enrique Bonilla>
2016-04-30 16:23:20 +0100

    Adds stuff

--- game.py
+++ game.py
@@ -1,1 +1,18 @@
-"Test"
+class User:
+	name = ""
+	area = "start"
+	#other information
+
+	#other methods
+
+class Area:
+	name = ""
+	description = ""
+	objects = list()
+	transitions = {}
+
+class Item:
+	name = ""
+	interactions = {}
+
+
